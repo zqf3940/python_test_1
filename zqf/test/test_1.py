4f9fc0fd92b4c103ebc9ccf2bc691a20c88c4494
zqf39402 <zqf3940@163.com>
2018-01-17 13:47:32 +0800

    1

---
+++ zqf/test/test_1.py
@@ -0,0 +1,9 @@
+print('{0:.3f}'.format(1.0/3));
+print('{0:_^11}'.format('Hello'))
+print('{name} wrote {book}'.format(name='Swaroop',book='A Byte of Python'))
+print('a',end=' ')
+print('b',end=' ')
+print('c')
+print("This is the first sentence. \
+      This is the second senttence.")
+print(r"Newlines are indicated by \n")
---
+++ zqf/test/test_2.py
@@ -0,0 +1,11 @@
+#2018-01-17 13:38:17
+i = 5
+print(i)
+i = i + 1
+print(i)
+s = '''this is a multi-line string.
+this is the second line.'''
+print(s)
+i = 5
+# print('value is',i)
+print('i repeat, the value is',i)
