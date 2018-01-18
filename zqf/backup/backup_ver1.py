1add300fac6c1f8a128605db6f1922e0aead985e
zqf39402 <zqf3940@163.com>
2018-01-18 16:37:49 +0800

    1

---
+++ zqf/backup/backup_ver1.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+import os
+import time
+
+source = [r'"c:\Users\zhongqif\zqf\note"']
+target_dir = r'C:\Users\zhongqif\zqf\test'
+
+target = target_dir + os.sep + \
+         time.strftime('%Y%M%D%H%M%S') + '.ZIP'
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+zip_command = 'zip -r {0} {1}'.format(target, ''.join(source))
+
+print('zip command is:')
+print(zip_command)
+print('running:')
+
+if os.system(zip_command) == 0:
+    print('successful backup to', target)
+else:
+    print('backup failed')
