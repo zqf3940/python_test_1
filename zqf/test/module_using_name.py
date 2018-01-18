cd3e1c270fb74eaef9a6eb6ed2ae158362dcc6ee
zqf39402 <zqf3940@163.com>
2018-01-18 13:57:43 +0800

    1

---
+++ zqf/test/module_using_name.py
@@ -0,0 +1,7 @@
+from math import sqrt
+print("Square root of 16 is", sqrt(16))
+
+if __name__ == '__main__':
+    print('This program is being run by itself')
+else:
+    print('I am being imported from another module')
