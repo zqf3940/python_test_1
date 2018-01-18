0e8e602ef7ee8aa2db6dab55cf2837199acc312a
zqf39402 <zqf3940@163.com>
2018-01-18 16:12:28 +0800

    1

---
+++ zqf/test2/ds_str_methods.py
@@ -0,0 +1,14 @@
+name = 'swaroop'
+
+if name.startswith('swa'):
+    print('tes, the string starts with "Swa"')
+
+if 'a' in name:
+    print('yes, is contains the string "a"')
+
+if name .find('war') != -1:
+    print('yes, it contains the string "war"')
+
+delimiter = '_*_'
+mylist = ['brazil', 'russia', 'india', 'china']
+print(delimiter.join(mylist))
